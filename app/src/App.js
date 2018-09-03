a08e1a0d49c7c5bae7a8328e37b72fa2107b0a9a
Jordan S <jpstanway@gmail.com>
2018-09-03 16:28:48 -0700

    return random quote and author name from array when quote btn is pressed

--- app/src/App.js
+++ app/src/App.js
@@ -1,38 +1,56 @@
 import React, { Component } from 'react';
+import QUOTES from './modules/quotes';
 
-const Quote = () => {
+
+const Quote = (props) => {
   return(
     <div id="quote-div">
-      <p id="text">Quote text goes here...</p>
+      <p id="text">{props.text}</p>
     </div>
   );
 };
 
-const Author = () => {
+const Author = (props) => {
   return(
     <div id="author-div">
-      <p id="author">Author name goes here ...</p>
-    </div>
-  );
-};
-
-const Controls = () => {
-  return(
-    <div id="button-div">
-      <button id="new-quote">New Quote</button>
-      <a href="twitter.com/intent/tweet"><button id="tweet-quote">Tweet Quote</button></a>
+      <p id="author">{props.name}</p>
     </div>
   );
 };
 
 class QuoteMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: QUOTES,
+      text: '',
+      author: ''
+    }
+
+    this.getQuote = this.getQuote.bind(this);
+  }
+
+  getQuote() {
+    let len = this.state.quotes.length;
+    let index = Math.floor(Math.random() * (len - 0 + 1)) + 0;
+    let obj = this.state.quotes[index];
+    this.setState({
+      text: obj.quote,
+      author: obj.author
+    });
+  }
 
   render() {
     return(
       <div id="quote-box">
-        <Quote />
-        <Author />
-        <Controls />
+        <Quote text={this.state.text}/>
+        <Author name={this.state.author}/>
+        <div id="button-div">
+          <button id="new-quote" onClick={this.getQuote}>New Quote</button>
+          <a href="twitter.com/intent/tweet" target="_blank">
+            <button id="tweet-quote">Tweet Quote</button>
+          </a>
+        </div>
       </div>
     );
   }
---
+++ app/src/modules/quotes.js
@@ -0,0 +1,84 @@
+const QUOTES = [
+    {
+      "quote": "quote text here 1",
+      "author": "author 1name here"
+    },
+    {
+      "quote": "quote text here 2",
+      "author": "author 4name here"
+    },
+    {
+      "quote": "quote text here 3",
+      "author": "author3 name here"
+    },
+    {
+      "quote": "quote text here 4",
+      "author": "author5 name here"
+    },
+    {
+      "quote": "quote text here 5",
+      "author": "author 6name here"
+    },
+    {
+      "quote": "quote text here 6",
+      "author": "author77 name here"
+    },
+    {
+      "quote": "quote text here 7",
+      "author": "author 88name here"
+    },
+    {
+      "quote": "quote text here 8",
+      "author": "author 66name here"
+    },
+    {
+      "quote": "quote text here 9",
+      "author": "author 55name here"
+    },
+    {
+      "quote": "quote text here 10",
+      "author": "author88 name here"
+    },
+    {
+      "quote": "quote text here 11",
+      "author": "author 151name here"
+    },
+    {
+      "quote": "quote text here 12",
+      "author": "author 353name here"
+    },
+    {
+      "quote": "quote text here 13",
+      "author": "author334 name here"
+    },
+    {
+      "quote": "quote text here 14",
+      "author": "author65 name here"
+    },
+    {
+      "quote": "quote text here15 ",
+      "author": "author 345name here"
+    },
+    {
+      "quote": "quote text here 156",
+      "author": "author 456456name here"
+    },
+    {
+      "quote": "quote text here 32",
+      "author": "author345345 name here"
+    },
+    {
+      "quote": "quote text here 54",
+      "author": "author 1562name here"
+    },
+    {
+      "quote": "quote text here24",
+      "author": "author 656name here"
+    },
+    {
+      "quote": "quote text here 65",
+      "author": "author 777name here"
+    }
+  ];
+
+  export default QUOTES;
