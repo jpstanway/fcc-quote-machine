05b789fcdeccd87e635e0c2c142bab55f3e29c2c
Jordan S <jpstanway@gmail.com>
2018-09-05 14:25:45 -0700

    complete quotes array with 20 quotes and authors

--- app/src/modules/quotes.js
+++ app/src/modules/quotes.js
@@ -1,83 +1,83 @@
 const QUOTES = [
     {
-      "quote": "quote text here 1",
-      "author": "author 1name here"
+      "quote": "We all make choices in life, but in the end our choices make us.",
+      "author": "Andrew Ryan, Bioshock"
     },
     {
-      "quote": "quote text here 2",
-      "author": "author 4name here"
+      "quote": "The right man in the wrong place can make all the difference in the world.",
+      "author": "G-Man, Half-Life 2"
     },
     {
-      "quote": "quote text here 3",
-      "author": "author3 name here"
+      "quote": "Wanting something does not give you the right to have it.",
+      "author": "Ezio Auditore, Assassin's Creed 2"
     },
     {
-      "quote": "quote text here 4",
-      "author": "author5 name here"
+      "quote": "It's a funny thing, ambition. It can take one to sublime heights or harrowing depths. And sometimes they are one and the same.",
+      "author": "Emily Kaldwin, Dishonored"
     },
     {
-      "quote": "quote text here 5",
-      "author": "author 6name here"
+      "quote": "It's easy to forget what a sin is in the middle of a battlefield.",
+      "author": "Solid Snake, Metal Gear Solid"
     },
     {
-      "quote": "quote text here 6",
-      "author": "author77 name here"
+      "quote": "NOTHING IS MORE BADASS THAN TREATING A WOMAN WITH RESPECT!",
+      "author": "Mr. Torgue, Borderlands 2"
     },
     {
-      "quote": "quote text here 7",
-      "author": "author 88name here"
+      "quote": "Death is inevitable. Our fear of it makes us play safe, blocks out emotion. It's a losing game. Without passion, you are already dead.",
+      "author": "Max Payne, Max Payne 2"
     },
     {
-      "quote": "quote text here 8",
-      "author": "author 66name here"
+      "quote": "I used to be an adventurer like you, until I took an arrow to the knee.",
+      "author": "Town Guard, Skyrim"
     },
     {
-      "quote": "quote text here 9",
-      "author": "author 55name here"
+      "quote": "Science isn't about why! It's about why not!",
+      "author": "Cave Johnson, Portal 2"
     },
     {
-      "quote": "quote text here 10",
-      "author": "author88 name here"
+      "quote": "I raised you, and loved you. I've given you weapons, taught you techniques, endowed you with knowledge. There's nothing more for me to give you. All that's left for you is to take my life.",
+      "author": "The Boss, Metal Gear Solid 3"
     },
     {
-      "quote": "quote text here 11",
-      "author": "author 151name here"
+      "quote": "If history is to change, let it change. If the world is to be destroyed, so be it. If my fate is to die, I must simply laugh.",
+      "author": "Magus, Chrono Trigger"
     },
     {
-      "quote": "quote text here 12",
-      "author": "author 353name here"
+      "quote": "War is where the young and stupid are tricked by the old and bitter into killing each other.",
+      "author": "Niko Bellic, GTA IV"
     },
     {
-      "quote": "quote text here 13",
-      "author": "author334 name here"
+      "quote": "No matter how dark the night, morning always comes, and our journey begins anew.",
+      "author": "Lulu, Final Fantasy X"
     },
     {
-      "quote": "quote text here 14",
-      "author": "author65 name here"
+      "quote": "What is a man? A miserable little pile of secrets.",
+      "author": "Dracula, Castlevania"
     },
     {
-      "quote": "quote text here15 ",
-      "author": "author 345name here"
+      "quote": "Good men mean well. We just don't always end up doing well.",
+      "author": "Isaac Clarke, Dead Space 3"
     },
     {
-      "quote": "quote text here 156",
-      "author": "author 456456name here"
+      "quote": "Don't wish it were easier, wish you were better.",
+      "author": "Chief, Animal Crossing"
     },
     {
-      "quote": "quote text here 32",
-      "author": "author345345 name here"
+      "quote": "It's time to kick ass and chew bubble gum... and I'm all outta gum.",
+      "author": "Duke Nukem, Duke Nukem 3D"
     },
     {
-      "quote": "quote text here 54",
-      "author": "author 1562name here"
+      "quote": "Even in dark times, we cannot relinquish the things that make us human.",
+      "author": "Khan, Metro 2033"
     },
     {
-      "quote": "quote text here24",
-      "author": "author 656name here"
+      "quote": "Stand in the ashes of a trillion dead souls, and ask the ghosts if honor matters. The silence is your answer.",
+      "author": "Javik, Mass Effect 3"
     },
     {
-      "quote": "quote text here 65",
-      "author": "author 777name here"
+      "quote": "What is better? To be born good or to overcome your evil nature through great effort?",
+      "author": "Paarthurnax, Skyrim"
     }
   ];
 
